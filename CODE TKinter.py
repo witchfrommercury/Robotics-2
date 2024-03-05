b816c64e44de61665e7be8f00e98201078c525dc
Kimberly Fabunan <157728066+witchfrommercury@users.noreply.github.com>
2024-03-05 11:10:07 +0800

    Update CODE TKinter.py

--- CODE TKinter.py
+++ CODE TKinter.py
@@ -1,1 +1,17 @@
+from tkinter import*
+from tkinter import message box
+from tkinter import PhotoIcon
+import numpy as py
+import roboticstoolbox as 
+from roboticstoolbox import
+
+#Creating a GUI window
+
+gui=Tk()
+gui.title("SCARA_V# Design Calculator")
+gui.resizable(False,False)
+gui.config(bg="green")
+
+gui.mainloop()
+
 
